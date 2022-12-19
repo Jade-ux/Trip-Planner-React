b9f1dd7a86aa057378527a730e48adbce09d7616
Jade Carvalho <62659009+Jade-ux@users.noreply.github.com>
2022-12-19 17:35:09 +0000

    Adds trip details form

---
+++ trip-planner/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NewTrip from './components/map-form/NewTrip';
+
+function App() {
+  return (
+    <div>
+      <NewTrip />
+    </div>
+  );
+}
+
+export default App;
---
+++ trip-planner/src/components/map-form/NewTrip.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import MapForm from './MapForm';
+// import './NewTrip.css';
+
+interface TripData {
+    country: string;
+    city: string;
+    fromDate: string;
+    toDate: string;
+}
+
+const NewTrip = () => {
+    const onSaveTripData = (enteredTripData: TripData) => {
+        const tripData = {
+            ...enteredTripData,
+            id: Math.random().toString()
+        };
+        console.log(tripData)
+    };
+
+    return <div className="new-trip">
+        <MapForm onSaveTripDataObj={onSaveTripData} />
+    </div>
+};
+
+export default NewTrip;
